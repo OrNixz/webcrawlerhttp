6396cad6a58f30d895411fec6675b801725e8452
OrNixz <ramadhanafif172@gmail.com>
2024-01-15 11:20:32 +0700

    printReport finished

---
+++ report.js
@@ -0,0 +1,29 @@
+function printReport(pages) {
+  console.log("============");
+  console.log("   Report  ");
+  console.log("============");
+  const sortedPages = sortPages(pages);
+  for (const sortedPage of sortedPages) {
+    const url = sortedPage[0];
+    const hits = sortedPage[1];
+    console.log(`Found ${hits} links to page: ${url}`);
+  }
+  console.log("============");
+  console.log(" End Report ");
+  console.log("============");
+}
+
+function sortPages(pages) {
+  const pagesArr = Object.entries(pages);
+  pagesArr.sort((a, b) => {
+    aHits = a[1];
+    bHits = b[1];
+    return bHits - aHits;
+  });
+  return pagesArr;
+}
+
+module.exports = {
+  sortPages,
+  printReport,
+};
