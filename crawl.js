880c962f8b164e5f6a81003d7e3bcb4f6fccffb8
OrNixz <ramadhanafif172@gmail.com>
2024-01-15 10:30:50 +0700

    crawl single page with error handler

--- crawl.js
+++ crawl.js
@@ -1,5 +1,30 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`Actively crawling: ${currentURL}}`);
+  try {
+    const resp = await fetch(currentURL);
+
+    if (resp.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${resp.status} on page: ${currentURL}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
+      return;
+    }
+
+    console.log(await resp.text());
+
+  } catch (error) {
+    console.log(`Error in fetch: ${error.message}`);
+  }
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -11,7 +36,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
         const urlObj = new URL(`${baseURL}${linkElement.href}`);
         urls.push(urlObj.href);
       } catch (error) {
-        console.log(`Error with relative url: ${error.message}`)
+        console.log(`Error with relative url: ${error.message}`);
       }
     } else {
       // absolute URL
@@ -19,7 +44,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
         const urlObj = new URL(linkElement.href);
         urls.push(urlObj.href);
       } catch (error) {
-        console.log(`Error with absolute url: ${error.message}`)
+        console.log(`Error with absolute url: ${error.message}`);
       }
     }
   }
@@ -38,4 +63,5 @@ function normalizeURL(urlString) {
 module.exports = {
   normalizeURL,
   getURLsFromHTML,
+  crawlPage,
 };
