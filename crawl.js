6218a1714d5c88e87e10eac193cb553a2c216fe1
OrNixz <ramadhanafif172@gmail.com>
2024-01-15 09:29:30 +0700

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,41 @@
-function normalizeURL(urlString) {
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1)
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative URL
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`Error with relative url: ${error.message}`)
+      }
+    } else {
+      // absolute URL
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`Error with absolute url: ${error.message}`)
+      }
     }
-    return hostPath
+  }
+  return urls;
+}
+
+function normalizeURL(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
-    normalizeURL
-}
+  normalizeURL,
+  getURLsFromHTML,
+};
